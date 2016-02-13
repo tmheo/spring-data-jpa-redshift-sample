4ff4eead579391130999993e937f9b99b53543f3
taemyung <tmheo74@gmail.com>
2016-02-13 18:44:11 +0900

    spring boot data jpa integration with aws redshift sample

---
+++ src/main/java/hello/Person.java
@@ -0,0 +1,40 @@
+package hello;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    private long id;
+
+    private String firstName;
+    private String lastName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
